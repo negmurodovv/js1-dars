e6110bd9a19d6dd2a5abd6ee97e2fd018a490394
Mr_Ulugbek <kinomaniya35@gmail.com>
2024-12-26 16:37:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let Username = prompt('Ismingiz')
+let Surname = prompt('Familiyangiz')
+let Age = prompt('Yoshingiz')
+let Address = prompt('Addressingiz')
+let Subject = prompt('Yoqtirgan Faningiz')
+
+
+console.log("Ulug'bek")
+console.log('Negmurodov')
+console.log('15')
+console.log('Sergeli Gulzor 175')
+console.log('English')
